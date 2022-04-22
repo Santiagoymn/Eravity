d5c538958e6a430d5d3c5bebc69bcaa243671adc
Santiagoymn <91144634+Santiagoymn@users.noreply.github.com>
2022-04-22 20:26:56 +0100

    Non-empty field validation

--- src/UploadUniversityForm.js
+++ src/UploadUniversityForm.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Helmet from 'react-helmet';
 import { db } from './firebase';
 import {collection, query, where, getDocs, addDoc, getDoc} from "firebase/firestore";
-import './uploadUniversityFormStyle.css';
+import styles from './uploadUniversityFormStyle.module.css';
 
 function UploadUniversityForm() {
     const[name, setName] = useState("");
@@ -11,24 +11,23 @@ function UploadUniversityForm() {
 
     const checkUniversity = (e) => {
         e.preventDefault();
-        const q = query(collection(db, "universities"), where("name", "==", name), where("city", "==", city), where("country", "==", country));
-        getDocs(q).then((querySnapshot) => {
+        if((name != "") && (city != "") && (country != "") ){
+            const q = query(collection(db, "universities"), where("name", "==", name), where("city", "==", city), where("country", "==", country));
+            getDocs(q).then((querySnapshot) => {
             if(querySnapshot.empty){
                 addDoc(collection(db, "universities"), {
                     name: name,
                     city: city,
                     country: country
             }).catch(error => alert(error.message))
-            alert("The university has been added. Thanks for supporting!");
-
+                alert("The university has been added. Thanks for supporting!");
             } else {
                 alert("The university already exists");
-
             }
-
-        })
-        
-        
+            })
+        }else{
+            alert("All fields are required");
+        }
     }
 
 
@@ -42,13 +41,13 @@ function UploadUniversityForm() {
             <title>Upload University</title>
         </Helmet>
         <form>
-            <input value={name} onChange={e => setName(e.target.value)} type="text" placeholder="name" id="name" className="inputShortText" required/>
+            <input value={name} onChange={e => setName(e.target.value)} type="text" placeholder="name" id="name" className={styles.inputShortText} required/>
 
-            <input value={city} onChange={e => setCity(e.target.value)} type="text" placeholder="city" id="city" className="inputShortText" required/>
+            <input value={city} onChange={e => setCity(e.target.value)} type="text" placeholder="city" id="city" className={styles.inputShortText} required/>
 
-            <input value={country} onChange={e => setCountry(e.target.value)} type="text" placeholder="country" id="country" className="inputShortText" required/>
+            <input value={country} onChange={e => setCountry(e.target.value)} type="text" placeholder="country" id="country" className={styles.inputShortText} required/>
 
-            <div className="buttonAccept"><input type="submit" onClick={checkUniversity} className="acceptBtn" value="Send"/></div>
+            <div className={styles.buttonAccept}><input type="submit" onClick={checkUniversity} className={styles.acceptBtn} value="Send"/></div>
 
         </form>
     </div>
