1b50a3e6be4ca54a22d6a99a900abd766ef397cb
Santiagoymn <91144634+Santiagoymn@users.noreply.github.com>
2022-04-22 17:08:01 +0100

    Create HomePage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
+import HomePage from './HomePage';
 import Register from './Register';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+const Stack = createNativeStackNavigator();
 function App() {
   return(
-    <div className='title'>
-      <Register />
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomePage} />
+        <Stack.Screen name="Register" component={Register} />
 
-    </div>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 
 
