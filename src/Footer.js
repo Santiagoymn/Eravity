913382f3c9ca71cc002dd712c640813f2a25eb2e
Santiagoymn <91144634+Santiagoymn@users.noreply.github.com>
2022-04-24 18:08:20 +0100

    Correction class names

--- src/Footer.js
+++ src/Footer.js
@@ -10,11 +10,11 @@ function Footer( {navigation} ){
                 <meta charset="UTF-8"/>
                 <title>Footer</title>
             </Helmet>
-            <footer className="Footer_footer">
-                <span className="Footer_logosRedesSociales">
-                    <a title="Facebook" href="https://www.facebook.com/"><img src="images/facebook.png" className="Footer_logoFacebook" alt="Logo Facebook"/></a>
-                    <a title="Instagram" href="https://www.instagram.com/"><img src="images/instagram.png" className="Footer_logoInstagram" alt="Logo Instagram"/></a>
-                    <a title="Twitter" href="https://twitter.com/"><img src="images/twitter.png" className="Footer_logoTwitter" alt="Logo Twitter"/></a>
+            <footer className="Footer__footer">
+                <span className="Footer__logosRedesSociales">
+                    <a title="Facebook" href="https://www.facebook.com/"><img src="images/facebook.png" className="Footer__logoFacebook" alt="Logo Facebook"/></a>
+                    <a title="Instagram" href="https://www.instagram.com/"><img src="images/instagram.png" className="Footer__logoInstagram" alt="Logo Instagram"/></a>
+                    <a title="Twitter" href="https://twitter.com/"><img src="images/twitter.png" className="Footer__logoTwitter" alt="Logo Twitter"/></a>
                 </span>
             </footer>
         </div>
