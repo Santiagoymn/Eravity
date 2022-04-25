3a50bb0fb3590a7dc07c0441a8c61d4be32079f5
María Naranjo Almeida <maria2000naranjo@gmail.com>
2022-04-25 17:09:44 +0100

    Changed DegreeProfile styles

--- src/DegreeProfilePage.js
+++ src/DegreeProfilePage.js
@@ -1,56 +1,55 @@
-import React, {Component, useEffect, useState} from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import $ from 'jquery';
 import { auth, db } from './firebase';
 import { doc, setDoc, docRef, getDoc, getDocs, collection } from "firebase/firestore";
 
 import './degreeProfilePage.css';
-import './generalStyle.css';
 import './assets/jquery.star-rating-svg';
 import './assets/star-rating-svg.css';
 
 function DegreeProfilePage() {
-	const {id} = useParams();
-	const[degree, setDegree] = useState([]);
-    const[university, setUniversity] = useState([]);
-	const[subjects, setSubjects] = useState([]);
+    const { id } = useParams();
+    const [degree, setDegree] = useState([]);
+    const [university, setUniversity] = useState([]);
+    const [subjects, setSubjects] = useState([]);
 
-	
 
-	const loadDegree = async() => {
 
-		const docRef = doc(db, "degrees", id)
-			getDoc(docRef).then((docSnap) => {
-				if (docSnap.exists()) {
-					setDegree(docSnap.data());
-                    loadUniveristy(docSnap.data().universityId);
-					loadSubjects(docSnap.data().subjects);	
-				} 
-				else {
-					console.log("No such document!");
-				}
-			})
+    const loadDegree = async () => {
 
-	}
+        const docRef = doc(db, "degrees", id)
+        getDoc(docRef).then((docSnap) => {
+            if (docSnap.exists()) {
+                setDegree(docSnap.data());
+                loadUniveristy(docSnap.data().universityId);
+                loadSubjects(docSnap.data().subjects);
+            }
+            else {
+                console.log("No such document!");
+            }
+        })
+
+    }
 
     const loadUniveristy = (o) => {
         const docRef = doc(db, "universities", o);
-            getDoc(docRef).then((docSnap) => {
+        getDoc(docRef).then((docSnap) => {
 
-                if (docSnap.exists()) {
-                    setUniversity(docSnap.data().name)
-                } 
-                else {
-                    console.log("No such document!");
-                }
+            if (docSnap.exists()) {
+                setUniversity(docSnap.data().name)
+            }
+            else {
+                console.log("No such document!");
+            }
 
-            })
+        })
     }
 
-	const loadSubjects =(o) => {
-		var keys = Object.keys(o).map((key) => [key]);
-        
-        for(let i=0; i<keys.length; i++){
+    const loadSubjects = (o) => {
+        var keys = Object.keys(o).map((key) => [key]);
+
+        for (let i = 0; i < keys.length; i++) {
 
             const docRef = doc(db, "subjects", keys[i][0]);
             getDoc(docRef).then((docSnap) => {
@@ -58,82 +57,85 @@ function DegreeProfilePage() {
                 if (docSnap.exists()) {
                     setSubjects(subjects => [...subjects, docSnap.data()])
                     console.log(subjects);
-                } 
+                }
                 else {
                     console.log("No such document!");
                 }
 
             })
         }
-			
-	}	
+
+    }
 
 
-	useEffect(() => {
-		loadDegree();
-	}, [])
+    useEffect(() => {
+        loadDegree();
+    }, [])
 
 
-	useEffect(() => {
-		$(".my-rating-4").starRating({
-			totalStars: 5,
-			starShape: 'rounded',
-			starSize: 40,
-			emptyColor: 'lightgray',
-			hoverColor: '#6096BA',
-			activeColor: 'green',
-			useGradient: false
-		})
-	}, [])
-	
-		
-	return(
-		<div>
-            <div class="universityDegree">
-                <div class="universityName"> {university} </div>
-                <div class="degreeName"> {degree.name} </div>
-            </div>
-            <article class="year">
-                <div class="headerYear">
-                    <div class="text">Year 1</div>
+    useEffect(() => {
+        $(".my-rating-4").starRating({
+            totalStars: 5,
+            starShape: 'rounded',
+            starSize: 40,
+            emptyColor: 'lightgray',
+            hoverColor: '#6096BA',
+            activeColor: 'green',
+            useGradient: false
+        })
+    }, [])
+
+
+    return (
+        <div>
+            <div className="DegreeProfile__uniContainer">
+                <div className="DegreeProfile__universityDegree">
+                    <div className="DegreeProfile__universityName"> {university} </div>
+                    <div className="DegreeProfile__degreeName"> {degree.name} </div>
                 </div>
-                <div class="semesters">
-                    <table class="tableSemester">
+            </div>
+
+            <article className="DegreeProfile__year">
+                <div className="DegreeProfile__headerYear">
+                    <div className="DegreeProfile__text">Year 1</div>
+                </div>
+                <div className="DegreeProfile__semesters">
+                    <table className="DegreeProfile__tableSemester">
                         <thead>
                             <tr>
-                                <th colspan="1" class="semester">First Semester</th>
+                                <th colspan="1" className="DegreeProfile__semester">First Semester</th>
                             </tr>
                         </thead>
                         <tbody>
                             {subjects.map((subject) => (
                                 <tr>
-                                    <td class="subjects">{subject.name}</td>
+                                    <td className="DegreeProfile__subjects">{subject.name}</td>
                                 </tr>
                             ))}
                         </tbody>
                     </table>
-                    <table class="tableSemester">
+                    <table className="DegreeProfile__tableSemester">
                         <thead>
-                        <tr>
-                            <th colspan="1" class="semester">Second Semester</th>
-                        </tr>
+                            <tr>
+                                <th colspan="1" className="DegreeProfile__semester">Second Semester</th>
+                            </tr>
                         </thead>
                         <tbody>
-                        <tr>
-                            <td class="subjects">Subject 1</td>
-                        </tr>
-                        <tr>
-                            <td class="subjects">Subject 2</td>
-                        </tr>
-                        <tr>
-                            <td class="subjects">Subject 3</td>
-                        </tr>
-                        <tr>
-                            <td class="subjects">Subject 4</td>
-                        </tr>
-                        <tr>
-                            <td class="subjects">Subject 5</td>
-                        </tr>
+                            <tr>
+                                <td className="DegreeProfile__subjects">Subject 1</td>
+                            </tr>
+                            <tr>
+                                <td className="DegreeProfile__subjects">Subject 2</td>
+                            </tr>
+                            <tr>
+                                <td className="DegreeProfile__subjects">Subject 3</td>
+                            </tr>
+                            <tr>
+                                <td className="DegreeProfile__subjects">Subject 4</td>
+                            </tr>
+                            <tr>
+                                <td className="DegreeProfile__subjects">Subject 5</td>
+                            </tr>
                         </tbody>
                     </table>
                 </div>
