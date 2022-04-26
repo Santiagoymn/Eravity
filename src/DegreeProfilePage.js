284c735f941b66e42bb98854077c7d032b435cc2
meelbaperez <merima001@hotmail.com>
2022-04-26 11:50:58 +0100

    Update error in import

--- src/DegreeProfilePage.js
+++ src/DegreeProfilePage.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import $ from 'jquery';
-import { db } from './firebase';
+import { db, auth } from './firebase';
 import { doc, getDoc} from "firebase/firestore";
 
 import './degreeProfilePage.css';
